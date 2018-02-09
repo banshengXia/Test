484941b0cb7a25f52945ef186048cad4d56a5640
AndyXia <andyxiamac@andyxiademacbook-pro.local>
2018-02-09 10:47:29 +0800

    初始化项目

---
+++ MYGitDemo/MYGitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYGitDemo
+//
+//  Created by AndyXia on 2018/2/9.
+//  Copyright © 2018年 AndyXia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
